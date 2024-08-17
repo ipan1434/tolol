a6ce818c34bcf7c8523fb0d299ca772db33f5c6d
WaysGanz <nazwaalbani06@gmail.com>
2024-08-17 19:29:22 +0700

    config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -9,16 +9,16 @@ DEVS = list(map(int, os.getenv("DEVS", "5802669435").split()))
 
 API_ID = int(os.getenv("API_ID", ""))
 
-API_HASH = os.getenv("API_HASH", "")
+API_HASH = os.getenv("API_HASH", "7373690920")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "AAGSIo4oYue73L5aDz1LA3Emkx6kFFVbynk")
 
-OWNER_ID = int(os.getenv("OWNER_ID", ""))
+OWNER_ID = int(os.getenv("OWNER_ID", "6304392781"))
 
 BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002186564307 -1002182272135 ").split()))
 
 RMBG_API = os.getenv("RMBG_API", "a6qxsmMJ3CsNo7HyxuKGsP1o")
 
-MONGO_URL = os.getenv("MONGO_URL", "")
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://buyervalls:buyervalls@cluster0.ixfnxed.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT"))
