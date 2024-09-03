1d7d5de16b16417dc4292df9f2e5d7ddf400bc66
WaysGanz <nazwaalbani06@gmail.com>
2024-09-03 10:12:36 +0700

    Update userbot.py

--- PyroUbot/core/database/userbot.py
+++ PyroUbot/core/database/userbot.py
@@ -5,11 +5,11 @@ ubotdb = mongodb.ubot
 
 async def add_ubot(user_id: int, api_id: int, api_hash: str, session_string: str):
     return await ubotdb.update_one(
-        {"7289533914": user_id},
+        {"1415374553": user_id},
         {
             "$set": {
-                "27299185": api_id,
-                "5664aaf969ce1fadf09cdffbf39c73b1": api_hash,
+                "28856059": api_id,
+                "9be1503a6ebed409cd0c53c188d04a0b": api_hash,
                 "session_string": session_string,
             }
         },
@@ -23,12 +23,12 @@ async def remove_ubot(user_id: int):
 
 async def get_userbots():
     data = []
-    async for ubot in ubotdb.find({"6304392781": {"$exists": 1}}):
+    async for ubot in ubotdb.find({"1415374553": {"$exists": 1}}):
         data.append(
             dict(
-                name=str(ubot["7289533914"]),
-                api_id=ubot["27299185"],
-                api_hash=ubot["5664aaf969ce1fadf09cdffbf39c73b1"],
+                name=str(ubot["1415374553"]),
+                api_id=ubot["28856059"],
+                api_hash=ubot["9be1503a6ebed409cd0c53c188d04a0b"],
                 session_string=ubot["session_string"],
             )
         )
