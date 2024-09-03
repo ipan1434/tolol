a6818004a68c902cf43c6fc53cecc14aecbf0549
WaysGanz <nazwaalbani06@gmail.com>
2024-09-03 10:11:30 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -5,15 +5,15 @@ load_dotenv(".env")
 
 MAX_BOT = int(os.getenv("MAX_BOT", "60"))
 
-DEVS = list(map(int, os.getenv("DEVS", "7289533914").split()))
+DEVS = list(map(int, os.getenv("DEVS", "1415374553").split()))
 
-API_ID = int(os.getenv("API_ID", "27299185"))
+API_ID = int(os.getenv("API_ID", "28856059"))
 
-API_HASH = os.getenv("API_HASH", "5664aaf969ce1fadf09cdffbf39c73b1")
+API_HASH = os.getenv("API_HASH", "9be1503a6ebed409cd0c53c188d04a0b")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7188015488:AAH2Hw-YHOm_ThqxdPqv1BrEBYAae-rXapE")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7528751518:AAF6nwjGW2C20hAVCgOSgudtGAulVBqiRIo")
 
-OWNER_ID = int(os.getenv("OWNER_ID", "7289533914"))
+OWNER_ID = int(os.getenv("OWNER_ID", "1415374553"))
 
 BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002186564307 -1002182272135 ").split()))
 
