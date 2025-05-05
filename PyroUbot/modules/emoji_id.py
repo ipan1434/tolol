9ca5e4cdd88ccc795250bea80ba5128da2ca3753
WaysGanteng <wayshosting8@gmail.com>
2025-05-05 11:20:02 +0700

    Create emoji_id.py

---
+++ PyroUbot/modules/emoji_id.py
@@ -0,0 +1,13 @@
+from pyrogram import Client, filters
+
+app = Client("userbot")  # atau sesuai session kamu
+
+@app.on_message(filters.me & filters.text)
+async def get_emoji_id(client, message):
+    if message.entities:
+        for entity in message.entities:
+            if entity.type == "custom_emoji":
+                emoji_id = entity.custom_emoji_id
+                await message.reply(f"Emoji ID: <code>{emoji_id}</code>", quote=True)
+
+app.run()
